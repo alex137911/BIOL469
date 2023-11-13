d17e80af0ea7f77a581caf54a2986b3090480fef
Alex Chan <alex.chan1@uwaterloo.ca>
2023-11-13 00:43:33 -0500

    update (11/13/2023)

--- cancerGene_variantPrediction.py
+++ cancerGene_variantPrediction.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
@@ -14,17 +15,20 @@ os.chdir("C:/Users/acale/OneDrive/Documents/Waterloo BME/4A/BIOL 469/Final Proje
 cancerGenes_olapFinal = pd.read_csv('cancerGenes_olapFinal.tsv', sep='\t')
 
 # Process data
+# Converts categorical text data into a numerical format so that machine learning algorithms can process them
+# For example, if you have categories like ["Benign", "Malignant"], they might be converted to [0, 1].
 label_encoder = LabelEncoder()
 cancerGenes_olapFinal['generalOntology']          = label_encoder.fit_transform(cancerGenes_olapFinal['generalOntology'])
 cancerGenes_olapFinal['reclassifiedSignificance'] = label_encoder.fit_transform(cancerGenes_olapFinal['reclassifiedSignificance'])
 
-# Split data into training and testing sets
+# Split data into training and testing sets (70% training, 30% testing)
 x = cancerGenes_olapFinal[['generalOntology']]         # Features
 y = cancerGenes_olapFinal['reclassifiedSignificance']  # Target variable
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
 # Build the model
+# 100 decision trees created by random forest model
 clf = RandomForestClassifier(n_estimators=100)
 clf.fit(x_train, y_train)
 
@@ -33,7 +37,12 @@ y_pred = clf.predict(x_test)
 print(classification_report(y_test, y_pred))
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
-# Cross validation
+# Cross validation (5-fold cross-validation)
 scores = cross_val_score(clf, x, y, cv=5)
 print("Cross-validated scores:", scores)
+
+# Provides the average score across all folds
 print("Average score:", scores.mean())
+
+# Save model
+joblib.dump(clf, 'random_forest_model.pkl')
