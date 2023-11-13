905bc62976ef16a2de034d1723950e97d0907eaa
Alex Chan <alex.chan1@uwaterloo.ca>
2023-11-12 23:00:30 -0500

    update (11/12/2023)

--- cancerGene_variantFiltering.R
+++ cancerGene_variantFiltering.R
@@ -13,6 +13,8 @@ suppressMessages(library(tidyr))
 suppressMessages(library(vcfR))
 suppressMessages(library(dplyr))
 suppressMessages(library(GenomicRanges))
+suppressMessages(library(caret))
+suppressMessages(library(glmnet)) # For logistic regression model
 
 # -------------------------------------------------------------------
 # Import MANE data
