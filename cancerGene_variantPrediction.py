01cf4a5349a2b62f5ea6b6879cf16cf1c71e32e7
Alex Chan <alex.chan1@uwaterloo.ca>
2023-11-13 18:44:37 -0500

    update (11/13/2023)

--- cancerGene_variantPrediction.py
+++ cancerGene_variantPrediction.py
@@ -14,6 +14,7 @@ os.chdir("C:/Users/acale/OneDrive/Documents/Waterloo BME/4A/BIOL 469/Final Proje
 # Read in the data
 cancerGenes_olapFinal = pd.read_csv('cancerGenes_olapFinal.tsv', sep='\t')
 
+# -------------------------------------------------------------------
 # Process data
 # Convert categorical text data into a numerical format so that machine learning algorithms can process them
 # Create separate label encoders for each column
@@ -35,7 +36,7 @@ print("Reclassified Significance Categories:", significance_categories)
 
 # Split data into training and testing sets (70% training, 30% testing)
 x = cancerGenes_olapFinal[['generalOntology_encoded']]  # Features
-y = cancerGenes_olapFinal['reclassifiedSignificance']   # Target variable
+y = cancerGenes_olapFinal['reclassifiedSignificance_encoded']   # Target variable
 
 # Ensure that the same rows are always used for training and testing with random_state
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
@@ -50,6 +51,7 @@ y_pred = clf.predict(x_test)
 print(classification_report(y_test, y_pred))
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
+# -------------------------------------------------------------------
 # Cross validation (5-fold cross-validation)
 scores = cross_val_score(clf, x, y, cv=5)
 print("Cross-validated scores:", scores)
@@ -59,3 +61,26 @@ print("Average score:", scores.mean())
 
 # Save model
 joblib.dump(clf, 'random_forest_model.pkl')
+
+# -------------------------------------------------------------------
+# Ensure that label_encoder_significance is fitted with all categories
+label_encoder_significance.fit(cancerGenes_olapFinal['reclassifiedSignificance'])
+
+# Check the fitted labels
+print("Fitted Labels:", label_encoder_significance.classes_)
+
+# Predict on the entire dataset
+cancerGenes_olapFinal['predictedSignificance'] = clf.predict(cancerGenes_olapFinal[['generalOntology_encoded']])
+
+# Add names of the categories for easier interpretation using inverse transformation
+cancerGenes_olapFinal['predictedSignificance_category'] = label_encoder_significance.inverse_transform(cancerGenes_olapFinal['predictedSignificance'])
+
+# Analyze specific cases
+# For example, filter for synonymous mutations
+synonymous_mutations = cancerGenes_olapFinal[cancerGenes_olapFinal['generalOntology'] == 'Synonymous Variant']
+print(synonymous_mutations[['generalOntology', 'reclassifiedSignificance', 'predictedSignificance_category']])
+
+# Statistical Summary
+# Group by 'generalOntology' and see the distribution of predicted categories
+summary = cancerGenes_olapFinal.groupby('generalOntology')['predictedSignificance_category'].value_counts().unstack()
+print(summary)
